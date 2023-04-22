c03125d43630c88a33801ac870f8d1e7cd69f34d
shantdashjian <shantgdashjian@gmail.com>
2023-04-22 17:05:12 +0400

    Incorporate suggestions from code review: move html formation to the Dog class, move vars to the top, extract swipe method, surround condition with parentheses, add DOCTYPE declaration, disable buttons while waiting

--- Dog.js
+++ Dog.js
@@ -2,4 +2,22 @@ export default class Dog {
 	constructor(data) {
 		Object.assign(this, data)
 	}
+
+	getDogHtml() {
+		const { name, avatar, age, bio, hasBeenSwiped, hasBeenLiked } = this
+		const like = hasBeenLiked ? "" : "hidden"
+		const nope = hasBeenSwiped && !hasBeenLiked ? "" : "hidden"
+
+		return `
+			<img class="dog-img" src="${avatar}" alt="${name}">
+			<div class="dog-text flex">
+				<p class="dog-text-name-age" >${name}, ${age}</p>
+				<p class="dog-text-bio">${bio}</p>
+			</div>
+			<div class="like-nope-badges flex">
+				<img class="badge" id="like-badge" src="images/badge-like.png" alt="like" ${like}>
+				<img class="badge" id="nope-badge" src="images/badge-nope.png" alt="nope" ${nope}>
+			</div>
+		`
+	}
 }
--- index.js
+++ index.js
@@ -3,45 +3,32 @@ import Dog from './Dog'
 
 let currentIndex = 0
 const lastIndex = dogs.length - 1
-
-render(new Dog(dogs[currentIndex]))
-
-function nextDog() {
-	currentIndex = currentIndex == lastIndex ? 0 : currentIndex + 1
-	return new Dog(dogs[currentIndex])
-}
-
-function render(dog) {
-	const { name, avatar, age, bio, hasBeenSwiped, hasBeenLiked } = dog
-	const like = hasBeenLiked ? "" : "hidden"
-	const nope = hasBeenSwiped && !hasBeenLiked ? "" : "hidden"
-
-	document.getElementById('dog').innerHTML = `
-		<img class="dog-img" src="${avatar}" alt="${name}">
-		<div class="dog-text flex">
-			<p class="dog-text-name-age" >${name}, ${age}</p>
-			<p class="dog-text-bio">${bio}</p>
-		</div>
-		<div class="like-nope-badges flex">
-			<img class="badge" id="like-badge" src="images/badge-like.png" alt="like" ${like}>
-			<img class="badge" id="nope-badge" src="images/badge-nope.png" alt="nope" ${nope}>
-		</div>
-	`
-}
-
 const crossBtn = document.getElementById('cross-btn')
 const heartBtn = document.getElementById('heart-btn')
 
-crossBtn.addEventListener('click', () => {
-	dogs[currentIndex].hasBeenSwiped = true
-	dogs[currentIndex].hasBeenLiked = false
-	render(new Dog(dogs[currentIndex]))
-	setTimeout(() => render(nextDog()), 2000)
-})
+render(new Dog(dogs[currentIndex]))
 
-heartBtn.addEventListener('click', () => {
+function render(dog) {
+	document.getElementById('dog').innerHTML = dog.getDogHtml()
+}
+
+crossBtn.addEventListener('click', () => swipe(false))
+heartBtn.addEventListener('click', () => swipe(true))
+
+function swipe(liked) {
 	dogs[currentIndex].hasBeenSwiped = true
-	dogs[currentIndex].hasBeenLiked = true
+	dogs[currentIndex].hasBeenLiked = liked
 	render(new Dog(dogs[currentIndex]))
-	setTimeout(() => render(nextDog()), 2000)
-})
+	crossBtn.disabled = true
+	heartBtn.disabled = true
+	setTimeout(() => {
+		render(nextDog())
+		crossBtn.disabled = false
+		heartBtn.disabled = false
+	}, 2000)
+}
+
+function nextDog() {
+	currentIndex = (currentIndex == lastIndex) ? 0 : currentIndex + 1
+	return new Dog(dogs[currentIndex])
+}
