8411d2f13786e69a277613e8eef3b42378abe173
shantdashjian <shantgdashjian@gmail.com>
2023-04-29 06:34:00 +0400

    Add title and comment in HTML for UX and readability, refactor JavaScript for readability

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ import dogs from './data'
 import Dog from './Dog'
 
 let currentIndex = 0
-const lastIndex = dogs.length - 1
 const crossBtn = document.getElementById('cross-btn')
 const heartBtn = document.getElementById('heart-btn')
 
-render(new Dog(dogs[currentIndex]))
+render(currentDog())
 
 function render(dog) {
 	document.getElementById('dog').innerHTML = dog.getDogHtml()
@@ -16,19 +15,40 @@ crossBtn.addEventListener('click', () => swipe(false))
 heartBtn.addEventListener('click', () => swipe(true))
 
 function swipe(liked) {
-	dogs[currentIndex].hasBeenSwiped = true
-	dogs[currentIndex].hasBeenLiked = liked
-	render(new Dog(dogs[currentIndex]))
-	crossBtn.disabled = true
-	heartBtn.disabled = true
+	setSwiped(true);
+	setLiked(liked);
+	render(currentDog())
+	disableButtons();
 	setTimeout(() => {
 		render(nextDog())
-		crossBtn.disabled = false
-		heartBtn.disabled = false
+		enableButtons();
 	}, 2000)
 }
 
+function currentDog() {
+	return new Dog(dogs[currentIndex]);
+}
+
 function nextDog() {
+	const lastIndex = dogs.length - 1
 	currentIndex = (currentIndex == lastIndex) ? 0 : currentIndex + 1
-	return new Dog(dogs[currentIndex])
+	return currentDog()
+}
+
+function disableButtons() {
+	crossBtn.disabled = true
+	heartBtn.disabled = true
+}
+
+function enableButtons() {
+	crossBtn.disabled = false
+	heartBtn.disabled = false
+}
+
+function setSwiped(swiped) {
+	dogs[currentIndex].hasBeenSwiped = swiped
+}
+
+function setLiked(liked) {
+	dogs[currentIndex].hasBeenLiked = liked
 }
